5af0179ed51a72334b95fe1590b800a6034164c1
tyler <tyler@desktop>
2019-03-16 14:03:47 -0600

    Start rewritting logic to get todays auctions.

--- src/Program.cs
+++ src/Program.cs
@@ -50,33 +50,17 @@ namespace PriceGrabber
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240");
             IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options);
-            driver.Navigate().GoToUrl("https://www.copart.com/salesListResult/");
+            driver.Navigate().GoToUrl("https://www.copart.com/todaysAuction/");
 
             // Wait until the page loads
             IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30.00));
             wait.Until(d => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
 
-            var rows = driver.FindElements(By.XPath("//*[@id='clientSideDataTable']/tbody/tr"));
-
-            // Date regex to match: 02/23/1999
-            Regex reg = new Regex(@"\b\d{2}\/\d{2}\/\d{4}\b");
-            foreach (IWebElement r in rows)
-            {
-                // Example rowText:
-                //"08:00 AM MDT GA - Atlanta East SOUTH EAST 03/14/2019 03/18/2019 Future"
-                Match m = reg.Match(r.Text);
-                if(m.Success)
-                {
-                    // Time is always at the beginning, just rip it off
-                    string time = r.Text.Substring(0, 8);
-                    string date = m.Value;
-                    DateTime dt = DateTime.ParseExact($"{date} {time}", "MM/dd/yyyy hh:mm tt", null);
-                    if(dt > DateTime.Now)
-                    {
-                        Console.WriteLine($"Next Auction: {dt}.");
-                    }
-                }
-            }
+            // Get all the rows that define todays auctions
+            var auctions = driver.FindElements(By.XPath("//*[@id='auctionLiveNow-datatable']/tbody/tr"));
+            auctions.ToList().ForEach(auction => {
+                
+            });
 
             driver.Dispose();
 
